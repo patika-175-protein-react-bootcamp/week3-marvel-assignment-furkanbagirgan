d5c57f588de424b3985fae4c771458857a41cf3e
Furkan Bağırgan <furkanbrgn@gmail.com>
2022-04-13 14:11:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -101,13 +101,13 @@ function App() {
 			<div className="dp-flex-row wd-100 footer">
 				<div className="dp-flex transition">
 					{/* Here the back button, next button and the page items are printed on the screen. */}
-					{activePage>3 && <span><i className="fa-solid fa-angle-left fa-2xl grey-color" onClick={()=>skipPage(activePage-1)}></i></span>}
+					{activePage>3 && <span><i className="fa-solid fa-angle-left fa-2xl grey-color trans-button" onClick={()=>skipPage(activePage-1)}></i></span>}
 					{
 						pages.map( (item,index) =>
 							<span key={index} className={activePage===item ? 'block-font active-page' : 'block-font foot-tran-span grey-color'} onClick={item!=='...' ? ()=>skipPage(item) : null}>{item}</span>
 						)
 					}
-					{activePage<pages[pages.length - 1]-3 && <span><i className="fa-solid fa-angle-right fa-2xl grey-color" onClick={()=>skipPage(activePage+1)}></i></span>}
+					{activePage<pages[pages.length - 1]-3 && <span><i className="fa-solid fa-angle-right fa-2xl grey-color trans-button" onClick={()=>skipPage(activePage+1)}></i></span>}
 				</div>
 			</div>
 		</div>
